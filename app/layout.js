47ad08c12d525945268d6b67437326995d693d36
LGomes360 <luke@lve360.com>
2025-08-21 18:21:02 -0600

    layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,8 @@
-// app/layout.js
+// /app/layout.js
+
 export const metadata = {
-  title: "LVE360",
-  description: "Live Better | Longevity · Vitality · Energy",
+  title: 'LVE360',
+  description: 'Personalized supplement platform powered by AI',
 };
 
 export default function RootLayout({ children }) {
