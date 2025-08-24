e7a3fe66f51612b0f619f73d196624edc66aadbf
LGomes360 <luke@lve360.com>
2025-08-24 14:33:57 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  // DO NOT set: output: 'export'  (that breaks API routes)
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
