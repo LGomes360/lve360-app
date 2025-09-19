f51e6ca7ea20bdda6c4a8337ee77b7c9a4b47e0d
LGomes360 <luke@lve360.com>
2025-09-19 15:45:47 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-red-500 flex items-center justify-center">
       <h1 className="text-5xl font-bold text-white">
+        <h1 className="text-5xl font-bold text-brand">Now Tailwind Really Works</h1>
         TAILWIND IS WORKING
       </h1>
     </main>
