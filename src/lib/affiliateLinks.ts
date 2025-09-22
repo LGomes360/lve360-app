d86435ed4d55ce3329584a6b37018825a98bb0d2
LGomes360 <luke@lve360.com>
2025-09-22 14:33:25 -0600

    Create affiliateLinks.ts

---
+++ src/lib/affiliateLinks.ts
@@ -0,0 +1,5 @@
+export async function enrichAffiliateLinks(markdown: string) {
+  // TODO: replace supplement names with affiliate links
+  // For now, just return unmodified markdown
+  return markdown;
+}
