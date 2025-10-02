90b5480ccd31b654be7864744b4d762769c2a0da
LGomes360 <luke@lve360.com>
2025-10-01 21:42:15 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -65,13 +65,23 @@ module.exports = {
           },
         },
       },
-      animation: {
+       animation: {
         fadeIn: "fadeIn 0.6s ease-in-out",
+        "fade-in-up": "fade-in-up 0.5s ease-out forwards",
+        "fade-out-down": "fade-out-down 0.5s ease-in forwards",
       },
       keyframes: {
         fadeIn: {
           "0%": { opacity: 0 },
           "100%": { opacity: 1 },
+        },
+        "fade-in-up": {
+          "0%": { opacity: "0", transform: "translateY(10px)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
+        },
+        "fade-out-down": {
+          "0%": { opacity: "1", transform: "translateY(0)" },
+          "100%": { opacity: "0", transform: "translateY(10px)" },
         },
       },
     },
